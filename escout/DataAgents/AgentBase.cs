89df0b66fc4b0c3ea2a5f326273676bfe37019ca
Sandro Ferreira <sandro_ferreira@outlook.pt>
2020-01-15 18:44:15 +0000

    Create database communication service

---
+++ escout/DataAgents/AgentBase.cs
@@ -0,0 +1,101 @@
+﻿using escout.Helpers;
+using Npgsql;
+using System;
+using System.Transactions;
+
+namespace escout.DataAgents
+{
+    public class AgentBase : IDisposable
+    {
+        private NpgsqlConnection npgsqlConnection;
+        private DataContext dataContext;
+        private int dbConnTimeout = 60;
+
+        public AgentBase()
+        {
+            OnInitializeSqlConnection(out npgsqlConnection);
+            OnInitializeDataContext(out dataContext);
+        }
+        public AgentBase(NpgsqlConnection npgsqlConnection)
+        {
+            this.npgsqlConnection = npgsqlConnection;
+            OnInitializeDataContext(out dataContext);
+        }
+
+        public AgentBase(AgentBase agent)
+        {
+            if (agent != null)
+            {
+                npgsqlConnection = agent.Connection;
+            }
+            else
+            {
+                OnInitializeSqlConnection(out npgsqlConnection);
+            }
+
+            OnInitializeDataContext(out dataContext);
+
+            if (agent != null)
+            {
+                dataContext.dbConnectionString = agent.dataContext.dbConnectionString;
+            }
+        }
+
+        ~AgentBase()
+        {
+            Dispose();
+        }
+
+        public int DbConnTimeout
+        {
+            get { return dbConnTimeout; }
+            set { dbConnTimeout = value; }
+        }
+
+        protected virtual void OnInitializeSqlConnection(out NpgsqlConnection npgsqlConnection)
+        {
+            npgsqlConnection = GetConnection();
+        }
+
+        protected virtual void OnInitializeDataContext(out DataContext dataContext)
+        {
+            dataContext = new DataContext();
+            dataContext.dbConnectionString = npgsqlConnection.ConnectionString;
+        }
+
+        public NpgsqlConnection Connection
+        {
+            get
+            {
+                return (npgsqlConnection);
+            }
+        }
+
+        public DataContext DataContext
+        {
+            get
+            {
+                return (dataContext);
+            }
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        private NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(Configurations.getNpgsqlConnectionString());
+        }
+
+        public TransactionScope CreateTransactionScope()
+        {
+            return (new TransactionScope(TransactionScopeOption.Required, new TransactionOptions()
+            {
+                IsolationLevel = System.Transactions.IsolationLevel.ReadCommitted,
+                Timeout = TransactionManager.MaximumTimeout
+            }));
+        }
+    }
+}
---
+++ escout/Helpers/Configurations.cs
@@ -0,0 +1,31 @@
+﻿using Npgsql;
+using System;
+
+namespace escout.Helpers
+{
+    public class Configurations
+    {
+        public static string getNpgsqlConnectionString()
+        {
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/')
+            };
+
+            return builder.ToString();
+        }
+
+        public static string getDatabaseScriptsPath()
+        {
+            return Environment.GetEnvironmentVariable("DATABASE_SCRIPTS_PATH");
+        }
+    }
+}

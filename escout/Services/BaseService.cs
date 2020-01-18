8b63d234c2d126971f267e36c4dfe9ae8d821e2e
Sandro Ferreira <sandro_ferreira@outlook.pt>
2020-01-18 19:57:29 +0000

    Authentication Service (#1)
    * Create database communication service
    
    * Create authentication service

---
+++ escout/Services/BaseService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace escout.Services
+{
+    public class BaseService : IDisposable
+    {
+        ~BaseService()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}

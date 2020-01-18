069f7dc30cf21c34ad112493485b8434fcab8a50
Sandro Ferreira <sandroacferreira@gmail.com>
2020-01-18 19:57:29 +0000

    Authentication Service (#1)
    * Create database communication service
    
    * Create authentication service

---
+++ escout/Controllers/AuthenticationController.cs
@@ -0,0 +1,53 @@
+﻿using escout.Models;
+using escout.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace escout.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        AuthService authService = new AuthService();
+
+        [HttpPost]
+        [Route("signIn")]
+        [AllowAnonymous]
+        public ActionResult<AuthData> SignIn(User userData)
+        {
+            var user = new User();
+            using (var service = new AuthService())
+            {
+                user = service.SignIn(userData);
+            }
+
+            if (user != null)
+            {
+                var token = authService.GetAuthData(user.id);
+                return token;
+            }
+            return new NotFoundResult();
+        }
+
+        [HttpPost]
+        [Route("signUp")]
+        [AllowAnonymous]
+        public ActionResult<string> SignUp(User user)
+        {
+            var userService = new UserService();
+            var authService = new AuthService();
+
+            var checkEmail = userService.CheckEmailExist(user.email);
+            if (checkEmail) return "Email already in use.";
+
+            var checkUsername = userService.CheckUsernameExist(user.username);
+            if (checkUsername) return "Username already in use.";
+
+            bool userAdded = authService.SignUp(user);
+            if (!userAdded) return "An error occurred while adding the user.";
+
+            return "User added successfully";
+        }
+    }
+}
---
+++ escout/Services/UserService.cs
@@ -0,0 +1,74 @@
+﻿using escout.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace escout.Services
+{
+    public class UserService : BaseService
+    {
+        DataContext db;
+
+        public UserService()
+        {
+            db = new DataContext();
+        }
+
+        public void AddUser(User usr)
+        {
+            var user = new User { username = usr.username, password = usr.password, email = usr.email };
+            db.users.Add(user);
+            db.SaveChanges();
+        }
+
+        public User GetUser(string username)
+        {
+            var user = db.users.FirstOrDefault(u => u.username == username);
+            Console.WriteLine(user.ToString());
+            return user;
+        }
+
+        public List<User> GetUsers()
+        {
+            return db.users.ToList<User>();
+        }
+
+        public User GetUserById(int id)
+        {
+            return db.users.FirstOrDefault(u => u.id == id);
+        }
+
+        public string ResetPassword(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ChangePassword(string username, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckEmailExist(string email)
+        {
+            var check = db.users.FirstOrDefault(u => u.email == email);
+            return check != null ? true : false;
+        }
+
+        public bool CheckUsernameExist(string username)
+        {
+            var check = db.users.FirstOrDefault(u => u.username == username);
+            return check != null ? true : false;
+        }
+
+        public bool CheckCredentials(string username, string email)
+        {
+            var check = db.users.FirstOrDefault(u => u.username == username || u.email == email);
+            return check != null ? true : false;
+        }
+
+        public bool SendEmailToUser(string username, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

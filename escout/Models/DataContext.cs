11b75399377e0db32ac43c0a5a7e368e8c5f3179
Sandro Ferreira <sandroacferreira@gmail.com>
2020-03-21 19:22:00 +0000

    Fix: game object nullable properties

--- escout/Models/DataContext.cs
+++ escout/Models/DataContext.cs
@@ -134,18 +134,18 @@ namespace escout.Models
         public string timeEnd { get; set; }
         public string homeColor { get; set; }
         public string visitorColor { get; set; }
-        public int homeScore { get; set; }
-        public int visitorScore { get; set; }
-        public int homePenaltyScore { get; set; }
-        public int visitorPenaltyScore { get; set; }
-        public int status { get; set; }
+        public int? homeScore { get; set; }
+        public int? visitorScore { get; set; }
+        public int? homePenaltyScore { get; set; }
+        public int? visitorPenaltyScore { get; set; }
+        public int? status { get; set; }
         public string type { get; set; }
         public string location { get; set; }
-        public int homeId { get; set; }
-        public int visitorId { get; set; }
+        public int? homeId { get; set; }
+        public int? visitorId { get; set; }
         public int? competitionId { get; set; }
         public int? imageId { get; set; }
-        public int userId { get; set; }
+        public int? userId { get; set; }
         public string created { get; set; }
         public string updated { get; set; }
     }

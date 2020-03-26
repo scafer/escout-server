58a997789356c06cd33c8710672b020420278100
Sandro Ferreira <sandro_ferreira@outlook.pt>
2020-03-26 23:09:29 +0000

    Add: Athlete and Authentication unit tests

--- escout/Startup.cs
+++ escout/Startup.cs
@@ -2,13 +2,13 @@ using escout.Helpers;
 using escout.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
-using Microsoft.AspNetCore.Http;
 
 namespace escout
 {
